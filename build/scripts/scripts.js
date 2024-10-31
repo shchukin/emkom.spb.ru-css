a75592bad789ee00cb30ef95d990053823aef88c
Anton Shchukin <a.a.shchukin@gmail.com>
2024-10-31 13:46:57 +0700

    code style and build

--- build/scripts/scripts.js
+++ build/scripts/scripts.js
@@ -116,7 +116,7 @@
                 autoHeight: true,
                 pagination: {
                     el: $carousel.querySelector('.carousel__pagination'),
-                    type: "bullets", /* переделать на fraction, если слишком много точек */
+                    type: "fraction", /* можно переделать на bullets, но когда добавлено много слайдов с проектами, они не влезают */
                     bulletClass: 'carousel__bullet',
                     bulletActiveClass: 'carousel__bullet--current',
                     clickable: true
@@ -132,7 +132,7 @@
                 autoHeight: true,
                 pagination: {
                     el: $carousel.querySelector('.carousel__pagination'),
-                    type: "bullets", /* переделать на fraction, если слишком много точек */
+                    type: "fraction", /* можно переделать на bullets или сделать всю секцию false */
                     bulletClass: 'carousel__bullet',
                     bulletActiveClass: 'carousel__bullet--current',
                     clickable: true
@@ -170,17 +170,55 @@
         if( $carousel.classList.contains('carousel--js-init-folder') ) {
             new Swiper($carousel.querySelector('.swiper'), {
                 slidesPerView: 2,
-                slidesPerGroup: 2,
+                slidesPerGroup: 1,
                 spaceBetween: 16,
                 autoHeight: true,
-                speed: 800,
-                loop: true, // а чтобы слева, до первого айтема не было дыры приходится зацикливаться
+                speed: 600,
+                loop: true,
                 autoplay: {
-                    delay: 1000,
+                    delay: 2000,
                 },
                 pagination: {
                     el: $carousel.querySelector('.carousel__pagination'),
-                    type: "bullets", /* переделать на fraction, если слишком много точек */
+                    type: "fraction", /* можно переделать на bullets или сделать всю секцию false */
+                    bulletClass: 'carousel__bullet',
+                    bulletActiveClass: 'carousel__bullet--current',
+                    clickable: true
+                },
+                navigation: {
+                    prevEl: $carousel.querySelector('.carousel__button--prev'),
+                    nextEl: $carousel.querySelector('.carousel__button--next'),
+                    disabledClass: 'carousel__button--disabled',
+                },
+                breakpoints: {
+                    500: {
+                        slidesPerView: 3,
+                        slidesPerGroup: 1,
+                    },
+                    740: {
+                        slidesPerView: 3,
+                        slidesPerGroup: 1,
+                        spaceBetween: 54,
+                    },
+                    1850: {
+                        slidesPerView: 5,
+                        slidesPerGroup: 1,
+                        spaceBetween: 84,
+                    }
+                }
+            });
+        }
+
+        if( $carousel.classList.contains('carousel--js-init-gallery') ) {
+            new Swiper($carousel.querySelector('.swiper'), {
+                slidesPerView: 1,
+                slidesPerGroup: 1,
+                spaceBetween: 16,
+                autoHeight: true,
+                speed: 600,
+                pagination: {
+                    el: $carousel.querySelector('.carousel__pagination'),
+                    type: "fraction", /* можно переделать на bullets или сделать всю секцию false */
                     bulletClass: 'carousel__bullet',
                     bulletActiveClass: 'carousel__bullet--current',
                     clickable: true
@@ -197,19 +235,40 @@
                     },
                     740: {
                         slidesPerView: 3,
-                        slidesPerGroup: 1,
+                        slidesPerGroup: 3,
                         spaceBetween: 54,
                     },
                     1850: {
                         slidesPerView: 5,
-                        slidesPerGroup: 1,
+                        slidesPerGroup: 5,
                         spaceBetween: 84,
                     }
-                }
+                },
+                on: {
+                    init: function () {
+                        toggleNavigationAndPagination(this);
+                    },
+                    resize: function () {
+                        toggleNavigationAndPagination(this);
+                    },
+                },
             });
         }
 
     });
+
+
+    /* Детектим кейс, когда в свайпере недостаточно айтемов для прокрутки хотя бы один раз, и убираем стрелочки/пагинацию */
+    function toggleNavigationAndPagination(swiper) {
+        const totalSlides = swiper.slides.length;
+        const slidesPerView = swiper.params.slidesPerView;
+
+        if (totalSlides <= slidesPerView) {
+            swiper.el.closest('.carousel').classList.add('carousel--not-enough-slides');
+        } else {
+            swiper.el.closest('.carousel').classList.remove('carousel--not-enough-slides');
+        }
+    }
 
 
 
@@ -271,6 +330,166 @@
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top - 30
         }, 600);
+    });
+
+
+
+    /* Форма */
+
+    /* Уведомления */
+    document.querySelectorAll('.alert').forEach(function (alert) {
+        alert.addEventListener('click', function () {
+            alert.style.display = 'none';
+        });
+    });
+
+    document.addEventListener('keyup', function (event) {
+        if (event.keyCode === 27) {
+            document.querySelectorAll('.alert').forEach(function (alert) {
+                alert.style.display = 'none';
+            });
+        }
+    });
+
+
+    /* Сама форма */
+    document.querySelectorAll('[data-js-form]').forEach(function (form) {
+        const subscriptionInputs = form.querySelectorAll('.input, .choice');
+        const subscriptionSubmit = form.querySelector('[data-js-submit]');
+        const subscriptionSuccessAlert = document.querySelector('[data-js-alert-success]');
+        const subscriptionFailureAlert = document.querySelector('[data-js-alert-failure]');
+
+        /* Состояния инпутов (на время отправки формы инпуты должны блокироваться) */
+        function disableSubscriptionInputs() {
+            subscriptionInputs.forEach((input) => {
+                input.querySelector('.input__widget, .choice__widget').setAttribute('disabled', 'disabled');
+            });
+        }
+
+        function enableSubscriptionInputs() {
+            subscriptionInputs.forEach((input) => {
+                input.querySelector('.input__widget, .choice__widget').removeAttribute('disabled');
+            });
+        }
+
+        /* Очистка инпутов (после успешной отправки форму должны очищаться) */
+        function cleanSubscriptionInputs() {
+
+            subscriptionInputs.forEach((input) => {
+
+                /* Есть три варианта инпутов: */
+
+                // 1) Текстовые инпуты внутри компонента .input (это всё, что не тег <select>):
+                const textInputs = input.querySelector('.input__widget:not(select)');
+                if (textInputs) {
+                    textInputs.value = '';
+                }
+
+                // 2) <select> внутри .input
+                const selects = input.querySelector('select.input__widget');
+                if (selects) {
+                    const placeholderOption = Array.from(selects.options).find(option => option.getAttribute('value') === 'placeholder');
+                    if (placeholderOption) {
+                        selects.value = placeholderOption.value;
+                        input.classList.add('input--placeholder-is-chosen');
+                    }
+                }
+
+                // 3) .choice__widget
+                const choices = input.querySelector('.choice__widget');
+                if (choices) {
+                    choices.checked = false;
+                }
+            });
+        }
+
+        /* Состояния кнопки */
+        function changeSubmitStateToLoading() {
+            subscriptionSubmit.classList.add('button--loading');
+            subscriptionSubmit.setAttribute('disabled', 'disabled');
+        }
+
+        function changeSubmitStateToSuccess() {
+            subscriptionSubmit.classList.remove('button--loading');
+            subscriptionSubmit.classList.add('button--success');
+            subscriptionSubmit.setAttribute('disabled', 'disabled');
+        }
+
+        function changeSubmitStateToFailure() {
+            subscriptionSubmit.classList.remove('button--loading');
+            subscriptionSubmit.classList.add('button--warning');
+            subscriptionSubmit.setAttribute('disabled', 'disabled');
+        }
+
+        function changeSubmitStateToPristine() {
+            subscriptionSubmit.classList.remove('button--loading', 'button--success', 'button--warning');
+            subscriptionSubmit.removeAttribute('disabled');
+        }
+
+        /* Если пользователь начал взаимодействовать с инпутами, то убираем уведомления с прошлой попытки отправки: */
+        subscriptionInputs.forEach(input => input.addEventListener('input', () => {
+            subscriptionFailureAlert.style.display = 'none';
+        }));
+
+
+        /* Отправка */
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            /* Если с прошлой попытки висит уведомление об ошибке: */
+            subscriptionFailureAlert.style.display = 'none';
+
+            /* Начинаем отправку данных, для начала блокируем форму */
+            disableSubscriptionInputs();
+            changeSubmitStateToLoading();
+
+            /* Представим, что 3000ms отправляем данные */
+            setTimeout(function () {
+
+                /* ... дальше развилка, пусть для примера будет рандом 50/50: */
+
+                // Если данные успешно отправлены
+                if (Math.random() < 0.5) {
+
+                    // показываем зелёное уведомление:
+                    subscriptionSuccessAlert.style.display = 'block';
+
+                    // показываем галочку на кнопке:
+                    changeSubmitStateToSuccess();
+
+                    // и то и другое на 4.5 секунды:
+                    setTimeout(function () {
+                        subscriptionSuccessAlert.style.display = 'none';
+                        changeSubmitStateToPristine();
+                        enableSubscriptionInputs();
+
+                        //и очищаем форму:
+                        cleanSubscriptionInputs();
+                    }, 4500);
+
+                }
+
+                // Если произошла ошибка
+                else {
+
+                    // показываем красное уведомление
+                    subscriptionFailureAlert.style.display = 'block';
+
+                    // Показываем восклицательный знак на кнопке:
+                    changeSubmitStateToFailure();
+
+                    // В данном случае всего 2 секунды, чтобы пользователь мог быстро вернуться к работе с формой.
+                    // Уведомление в этом случае НЕ убираем -- пусть висит, пока пользователь не увидит и явно не закроет, или не начнёт заново заполнять форму / попытается отправить:
+                    setTimeout(function () {
+                        changeSubmitStateToPristine();
+                        enableSubscriptionInputs();
+                    }, 2000);
+
+                }
+
+            }, 3000);
+
+        });
     });
 
 })(jQuery);
